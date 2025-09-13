3144f9da6a885ec959f2966fa3119c8c3aa88673
250441100106crysolitiara <crysolisimanjuntak@gmail.com>
2025-09-13 11:43:26 +0700

    250441100106 crysoli tiara simanjuntak

---
+++ 250441100106/pelcod.py
@@ -0,0 +1,29 @@
+# # nama = "Crysoli Tiara" 
+# # nim = 250441100106
+# # semester = 1 
+
+# # print ("=======BIODATA MAHASISWA=======")
+# # print ("hallo,nama saya :"+ nama + "\n")
+# # print(f"NIM saya :{nim}\n")
+# # print("saya berkuliah di semester : {}".format(semester)
+
+# a = 10
+# b = 5
+# c = a + b # penjumlahan 
+# d = a - b # pengurangan 
+# e = a * b # perkalian 
+# f = a / b # pembagian
+# g = a % b # sisa bagi 
+# print("Hasil Penjumlahan:" , c)
+# print("Hasil Pengurangan:" , d)
+# print("Hasil Perkalian:" , e)
+# print("Hasil Pembagian:" , f)
+# print("Hasil Sisa Bagi:" , g)
+
+nama = input("Masukkan nama Anda: ")
+umur = input("Masukkan umur Anda: ")
+buah = input ("Masukkan buah favorit Anda: ")
+
+print("Nama saya adalah " + nama)
+print("Umur saya adalah " + umur + " tahun")
+print("Buah favorit saya adalah " + buah)
